3d6d3c5cad894cc64d5a54ba43c481b872b2a57a
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-08-17 18:54:09 +0530

    backend

--- backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
+++ backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
@@ -4,10 +4,11 @@ import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootConfiguration
 public class Application {
 
 	public static void main(String[] args) {
