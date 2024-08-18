716ef7cde18ca37b2b30e002599ba55acf7078a8
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-08-18 09:30:51 +0530

    test.app

--- backEnd/discovery_LMS/src/test/java/com/app/ApplicationTests.java
+++ backEnd/discovery_LMS/src/test/java/com/app/ApplicationTests.java
@@ -3,7 +3,9 @@ package com.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.discovery.Application;
+
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
 	@Test
