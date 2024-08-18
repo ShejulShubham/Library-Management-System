c84e14cdfec375ecfcd23dee0c23acf7a7828f51
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-08-18 14:10:01 +0530

    docker

--- backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
+++ backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
@@ -4,7 +4,6 @@ import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
