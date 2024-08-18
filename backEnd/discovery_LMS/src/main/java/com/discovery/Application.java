e0a2e879a23f4450dfece7a406c079339b545930
shejulshubham <shejulshubhamdeshmukh@gmail.com>
2024-08-18 13:59:36 +0530

    vs code commit

--- backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
+++ backEnd/discovery_LMS/src/main/java/com/discovery/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
